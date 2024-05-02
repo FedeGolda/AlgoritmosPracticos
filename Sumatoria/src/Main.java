5fe5a0b18abbdee223338c6bbecbef6d55ae3098
FedeGolda <federico.goldaracena@hotmail.com>
2024-05-02 20:02:33 -0300

    Agregado funcion Factorial

--- Sumatoria/src/Main.java
+++ Sumatoria/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Sumatoria: " + Sumatoria(2));
-        int[] vector = {1,2};
-        System.out.println("Sumatoria con Vector: " + SumatoriaVector(vector, vector.length - 1));
-        System.out.println("Multiplicacion: " + Multiplicar(2,10));
+        int numeroSumatoria = 2;
+        System.out.println("Sumatoria de " + numeroSumatoria + ": " + Sumatoria(numeroSumatoria));
+
+        int[] vector = {1, 2};
+        System.out.println("Sumatoria del vector " + Arrays.toString(vector) + ": " + SumatoriaVector(vector, vector.length - 1));
+
+        int baseMultiplicar = 2, multiplicador = 10;
+        System.out.println("Multiplicación de " + baseMultiplicar + " * " + multiplicador + ": " + Multiplicar(baseMultiplicar, multiplicador));
+
+        int numeroFactorial = 5;
+        System.out.println("Factorial de " + numeroFactorial + ": " + Factorial(numeroFactorial));
     }
 
     public static int Sumatoria(int n) {
@@ -18,11 +27,17 @@ public class Main {
         return v[i] + SumatoriaVector(v, i - 1);
     }
 
-
     public static int Multiplicar(int n, int m) {
-        if (n == 0 && m == 0)
+        if (m == 0)
             return 0;
+        else if (m == 1)
+            return n;
         return n + Multiplicar(n, m - 1);
     }
 
+    public static int Factorial(int n){
+        if(n == 0)
+            return 1;
+        return n * Factorial(n - 1);
+    }
 }
