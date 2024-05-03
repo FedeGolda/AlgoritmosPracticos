03d6a6be56d49556885a6a3dfd39fb1d45ec4f2e
FedeGolda <federico.goldaracena@hotmail.com>
2024-05-02 22:02:07 -0300

    Agregado funcion Fibonacci

--- Sumatoria/src/Main.java
+++ Sumatoria/src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Arrays;
 
 public class Main {
@@ -13,6 +15,16 @@ public class Main {
 
         int numeroFactorial = 5;
         System.out.println("Factorial de " + numeroFactorial + ": " + Factorial(numeroFactorial));
+
+        listar(5);
+        listar2(5);
+
+        int[] vector2 = {5, 2, 8, 3, 1};
+        int minimo = ValorMinimo(vector2, vector2.length - 1);
+        System.out.println("El valor mínimo es: " + minimo);
+
+        int numF = 5;
+        System.out.println("Fibonacci de " + numF + ": " + Fibonacci(numF));
     }
 
     public static int Sumatoria(int n) {
@@ -35,9 +47,50 @@ public class Main {
         return n + Multiplicar(n, m - 1);
     }
 
-    public static int Factorial(int n){
-        if(n == 0)
+    // PRE: n > 0
+
+    public static int Factorial(int n) {
+        if (n == 0)
             return 1;
         return n * Factorial(n - 1);
     }
+
+    public static void listar(int n) {
+        if (n > 0)
+            listar(n - 1);
+
+        System.out.println(n);
+    }
+
+    public static void listar2(int n) {
+        System.out.println(n);
+        if (n > 0)
+            listar(n - 1);
+    }
+
+
+    public static int ValorMinimo(int[] v, int i) {
+        if (i == 0) {
+            return v[0];
+        } else {
+            int minResto = ValorMinimo(v, i - 1);
+            if (v[i] < minResto) {
+                return v[i];
+            } else {
+                return minResto;
+            }
+        }
+    }
+
+
+    public static int Fibonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+
+        return (Fibonacci(n - 1) + Fibonacci(n - 2));
+    }
+
+
 }
